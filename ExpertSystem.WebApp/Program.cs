7de3df1ce04db83298733237007b3e949e43df6a
burstCode <yourewelcomex@gmail.com>
2026-09-16 00:50:03 +0300

    WebApp template

---
+++ ExpertSystem.WebApp/Program.cs
@@ -0,0 +1,24 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+WebApplication app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
+
+app.Run();
