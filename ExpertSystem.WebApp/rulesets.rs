f4f1bebab3df578ce0d9ed18a53190a269146002
burstCode <yourewelcomex@gmail.com>
2026-09-24 23:32:47 +0300

    chore: final version of default rulesets

--- ExpertSystem.WebApp/rulesets.rs
+++ ExpertSystem.WebApp/rulesets.rs
@@ -1,1 +1,1038 @@
-[{"Conditions":[{"Object":"процессор","Value":"intel core i3"}],"Consequence":{"Object":"видеокарта","Value":"nvidia geforce gt1030"}}]
+[
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Офисный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Базовый"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Мультимедийный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1080p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Базовый"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Мультимедийный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1440p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Средний"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Мультимедийный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "4K"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Средний"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Игровой"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Низкий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Средний"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Игровой"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Высокий"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Игровой"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Максимальный"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Рабочая станция"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Низкий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Средний"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Рабочая станция"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Высокий"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Назначение",
+        "Value": "Рабочая станция"
+      },
+      {
+        "Object": "Бюджет",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Класс сборки",
+      "Value": "Максимальный"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "Intel"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "Intel Core i3-12100"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "AMD"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "AMD Ryzen 5 5600G"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "Intel"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "Intel Core i5-12400F"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "AMD"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "AMD Ryzen 5 7500F"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "Intel"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "Intel Core i5-14600KF"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "AMD"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "AMD Ryzen 7 7700"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "Intel"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "Intel Core i7-14700K"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      },
+      {
+        "Object": "Платформа",
+        "Value": "AMD"
+      }
+    ],
+    "Consequence": {
+      "Object": "Процессор",
+      "Value": "AMD Ryzen 9 7900X"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "Intel Core i3-12100"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "LGA1700"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "Intel Core i5-12400F"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "LGA1700"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "Intel Core i5-14600KF"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "LGA1700"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "Intel Core i7-14700K"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "LGA1700"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "AMD Ryzen 5 5600G"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "AM4"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "AMD Ryzen 5 7500F"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "AM5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "AMD Ryzen 7 7700"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "AM5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Процессор",
+        "Value": "AMD Ryzen 9 7900X"
+      }
+    ],
+    "Consequence": {
+      "Object": "Сокет",
+      "Value": "AM5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM4"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR4"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM5"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR4"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR4"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Тип памяти",
+      "Value": "DDR5"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      }
+    ],
+    "Consequence": {
+      "Object": "Объём ОЗУ",
+      "Value": "16 ГБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Объём ОЗУ",
+      "Value": "16 ГБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Объём ОЗУ",
+      "Value": "32 ГБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Объём ОЗУ",
+      "Value": "64 ГБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR4"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Стандартный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "Intel B660 (DDR4), ATX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR4"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Компактный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "Intel B660 (DDR4), Mini-ITX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR5"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Стандартный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "Intel B760 (DDR5), ATX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "LGA1700"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR5"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Компактный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "Intel B760 (DDR5), Mini-ITX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM4"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR4"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Стандартный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "AMD B550, ATX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM4"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR4"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Компактный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "AMD B550, Mini-ITX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM5"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR5"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Стандартный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "AMD B650, ATX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Сокет",
+        "Value": "AM5"
+      },
+      {
+        "Object": "Тип памяти",
+        "Value": "DDR5"
+      },
+      {
+        "Object": "Форм-фактор",
+        "Value": "Компактный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Материнская плата",
+      "Value": "AMD B650, Mini-ITX"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Встроенная в процессор"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1080p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4060"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1440p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4060 Ti"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "4K"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4070"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1080p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4060 Ti"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1440p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4070 Super"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "4K"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4070 Ti Super"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1080p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4070 Super"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "1440p"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4070 Ti Super"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      },
+      {
+        "Object": "Разрешение монитора",
+        "Value": "4K"
+      }
+    ],
+    "Consequence": {
+      "Object": "Видеокарта",
+      "Value": "Nvidia GeForce RTX 4080 Super"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Встроенная в процессор"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "450 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4060"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "550 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4060 Ti"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "550 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4070"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "650 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4070 Super"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "650 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4070 Ti Super"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "750 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Видеокарта",
+        "Value": "Nvidia GeForce RTX 4080 Super"
+      }
+    ],
+    "Consequence": {
+      "Object": "Блок питания",
+      "Value": "850 Вт"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      }
+    ],
+    "Consequence": {
+      "Object": "Охлаждение",
+      "Value": "Боксовый кулер"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Охлаждение",
+      "Value": "Башенный кулер"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Охлаждение",
+      "Value": "Двухбашенный кулер"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Охлаждение",
+      "Value": "СЖО 360 мм"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Базовый"
+      }
+    ],
+    "Consequence": {
+      "Object": "Накопитель",
+      "Value": "NVMe SSD 512 ГБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Средний"
+      }
+    ],
+    "Consequence": {
+      "Object": "Накопитель",
+      "Value": "NVMe SSD 1 ТБ"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Высокий"
+      }
+    ],
+    "Consequence": {
+      "Object": "Накопитель",
+      "Value": "NVMe SSD 1 ТБ (PCIe 4.0)"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Класс сборки",
+        "Value": "Максимальный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Накопитель",
+      "Value": "NVMe SSD 2 ТБ (PCIe 4.0)"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Форм-фактор",
+        "Value": "Стандартный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Корпус",
+      "Value": "ATX Mid-Tower"
+    }
+  },
+  {
+    "Conditions": [
+      {
+        "Object": "Форм-фактор",
+        "Value": "Компактный"
+      }
+    ],
+    "Consequence": {
+      "Object": "Корпус",
+      "Value": "Mini-ITX"
+    }
+  }
+]
